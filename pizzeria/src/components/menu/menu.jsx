8c53ebeb55e4a53fac69da71dd822cb5e33558d4
22030678 <utm22030678@utma.edu.mx>
2024-07-03 18:25:43 -0600

    feat: se agrega carrito de compras a menu

--- pizzeria/src/components/menu/menu.jsx
+++ pizzeria/src/components/menu/menu.jsx
@@ -1,71 +1,86 @@
-import React, { useState } from 'react';
-import { Card, Button } from 'react-bootstrap';
-import { FaChevronDown, FaChevronUp } from 'react-icons/fa';
+import React, { useState, useEffect } from 'react';
+import { Card, Button, Container, Row, Col, ListGroup } from 'react-bootstrap';
 
 const Menu = () => {
-  const [selectedPizza, setSelectedPizza] = useState(null);
+  const [pizzas, setPizzas] = useState([]);
+  const [carrito, setCarrito] = useState([]);
+  const divisa = '€';
 
-  const pizzas = [
-    { name: 'Mexicana', ingredients: ['Jitomate', 'Queso', 'Jamón', 'Jalapeño', 'Cebolla', 'Chorizo'], image: 'https://www.cheesepizza.com.mx/img/pizzas/mexicana.png' },
-    { name: 'Country', ingredients: ['Salami', 'Peperoni', 'Tocino', 'Champiñones', 'Cebolla', 'Queso'], image: 'https://www.cheesepizza.com.mx/img/pizzas/Country.png' },
-    { name: 'Hawaiana', ingredients: ['Queso', 'Jamón', 'Piña'], image: 'https://www.cheesepizza.com.mx/img/pizzas/Hawai.png' },
-    { name: 'Ranchera', ingredients: ['Queso', 'Frijoles', 'Chorizo', 'Aguacate', 'Jalapeño'], image: 'https://www.cheesepizza.com.mx/img/pizzas/ranchera.png' },
-    { name: 'Meat', ingredients: ['Queso', 'Jamón', 'Chorizo', 'Peperoni', 'Salami'], image: 'https://www.cheesepizza.com.mx/img/pizzas/meat.png' },
-    { name: 'Vegetariana', ingredients: ['Queso', 'Champiñon', 'Piña', 'Pimiento', 'Aceituna'], image: 'https://www.cheesepizza.com.mx/img/pizzas/vegetariana.png' },
-  ];
+  useEffect(() => {
+    const baseDeDatos = [
+      { id: 1, nombre: 'Mexicana', precio: 10, imagen: 'https://www.cheesepizza.com.mx/img/pizzas/mexicana.png' },
+      { id: 2, nombre: 'Country', precio: 12, imagen: 'https://www.cheesepizza.com.mx/img/pizzas/Country.png' },
+      { id: 3, nombre: 'Hawaiana', precio: 8, imagen: 'https://www.cheesepizza.com.mx/img/pizzas/Hawai.png' },
+      { id: 4, nombre: 'Ranchera', precio: 11, imagen: 'https://www.cheesepizza.com.mx/img/pizzas/ranchera.png' },
+      { id: 5, nombre: 'Meat', precio: 14, imagen: 'https://www.cheesepizza.com.mx/img/pizzas/meat.png' },
+      { id: 6, nombre: 'Vegetariana', precio: 9, imagen: 'https://www.cheesepizza.com.mx/img/pizzas/vegetariana.png' },
+    ];
+    setPizzas(baseDeDatos);
+  }, []);
 
-  const handleToggleIngredients = (pizza) => {
-    if (selectedPizza && selectedPizza.name === pizza.name) {
-      setSelectedPizza(null);
-    } else {
-      setSelectedPizza(pizza);
-    }
+  const agregarProductoAlCarrito = (id) => {
+    setCarrito([...carrito, id]);
+  };
+
+  const borrarProductoDelCarrito = (id) => {
+    setCarrito(carrito.filter((item) => item !== id));
+  };
+
+  const vaciarCarrito = () => {
+    setCarrito([]);
+  };
+
+  const calcularTotal = () => {
+    return carrito.reduce((total, id) => {
+      const pizza = pizzas.find((p) => p.id === id);
+      return total + pizza.precio;
+    }, 0).toFixed(2);
+  };
+
+  const renderizarCarrito = () => {
+    const carritoSinDuplicados = [...new Set(carrito)];
+    return carritoSinDuplicados.map((id, index) => {
+      const pizza = pizzas.find((p) => p.id === id);
+      const cantidad = carrito.filter((itemId) => itemId === id).length;
+      return (
+        <ListGroup.Item key={index} className="d-flex justify-content-between align-items-center">
+          {`${cantidad} x ${pizza.nombre} - ${pizza.precio}${divisa}`}
+          <Button variant="danger" onClick={() => borrarProductoDelCarrito(id)}>X</Button>
+        </ListGroup.Item>
+      );
+    });
   };
 
   return (
-    <div className="menu md-4">
-      <h1 style={{ textAlign: 'center', marginBottom:'50px' }}>Menú</h1>
-      <div className="container md-4"
-        style={{
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          backgroundRepeat: 'no-repeat',
-          borderRadius: '10px',
-        }}
-      >
-        <div className="row justify-content-center">
-          {pizzas.map((pizza, index) => (
-            <div key={index} className="col-md-4 mb-4 d-flex justify-content-center">
-              <Card style={{ width: '18rem', border: 'none' }}>
-                <Card.Title style={{ fontSize: '1.5rem', fontWeight: 'bold', color: '#ff6347' }}>
-                  {pizza.name}
-                </Card.Title>
-                <Card.Img
-                  variant="top"
-                  src={pizza.image}
-                  style={{ cursor: 'pointer' }}
-                  onClick={() => handleToggleIngredients(pizza)}
-                />
-                <Card.Body style={{ minHeight: selectedPizza && selectedPizza.name === pizza.name ? '100px' : '150px' }}>
-                  <Button
-                    variant="primary"
-                    style={{ backgroundColor: '#ff6347', borderColor: '#ff6347' }}
-                    onClick={() => handleToggleIngredients(pizza)}
-                  >
-                    Ver Ingredientes {selectedPizza && selectedPizza.name === pizza.name ? <FaChevronUp /> : <FaChevronDown />}
-                  </Button>
-                  {selectedPizza && selectedPizza.name === pizza.name && (
-                    <div style={{ marginTop: '10px', backgroundColor:'white', borderRadius:'7px' }}>
-                      <p>{selectedPizza.ingredients.join(', ')}</p>
-                    </div>
-                  )}
-                </Card.Body>
-              </Card>
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>
+    <Container>
+      <Row>
+        <Col sm={8}>
+          <Row>
+            {pizzas.map((pizza) => (
+              <Col sm={4} key={pizza.id}>
+                <Card className="mb-4">
+                  <Card.Img variant="top" src={pizza.imagen} />
+                  <Card.Body>
+                    <Card.Title>{pizza.nombre}</Card.Title>
+                    <Card.Text>{`${pizza.precio}${divisa}`}</Card.Text>
+                    <Button variant="primary" onClick={() => agregarProductoAlCarrito(pizza.id)}>Agregar</Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </Col>
+        <Col sm={4}>
+          <h2>Carrito</h2>
+          <ListGroup>
+            {renderizarCarrito()}
+          </ListGroup>
+          <hr />
+          <p className="text-right">Total: <span>{calcularTotal()}</span>&euro;</p>
+          <Button variant="danger" onClick={vaciarCarrito}>Vaciar</Button>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
